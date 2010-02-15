df8a8aa4efa652ada3bfcd0cdc456062e4376710
Michael JasonSmith <mpj17@onlinegroups.net>
2010-02-15 13:39:57 +1100

    Make password_set a utility.

--- gs/profile/signup/request_registration.py
+++ gs/profile/signup/request_registration.py
@@ -154,7 +154,7 @@ None.'''
         changedProfile = email.split('@')[0] != userInfo.name
         verified = emailUser.get_verifiedEmailAddresses() != []
 
-        if not self.passwordSet(emailUser):
+        if not password_set(emailUser):
             # Go to the Set Password page. This is no worse than
             # someone just registering with someone else's email
             # address. (That is, pointless as the address cannot be 
@@ -187,40 +187,6 @@ None.'''
             
         retval = NextPageInfo(uri, m, systemLoginRequired)
         assert isinstance(retval, NextPageInfo)
-        return retval
-
-    def passwordSet(self, user):
-        '''Check if a password has ever been set.
-        
-Description
------------
-
-We cannot check the password itself to see if the password is blank,
-because it may be hashed in all sorts of ways, so a blank password
-does no look blank. Instead this little method looks in the audit-trail
-for a profile-event that corresponds to a member setting a password.
-
-Returns
--------
-
-Boolean, True if a password has been set.
-
-Side Effects
-------------
-
-None.
-
-Acknowledgements
----------------
-
-Thanks to Alice for suggesting that I trawl the audit-logs for the 
-set-password events.'''
-        q = AuditQuery(self.context.zsqlalchemy)
-        items = q.get_instance_user_events(user.getId(), limit=128)
-        setPasswordItems = [i for i in items 
-                            if ((i['subsystem'] == SUBSYSTEM)
-                                and (i['code'] == SET_PASSWORD))]
-        retval = bool(setPasswordItems)
         return retval
 
     def createUser(self, email):
@@ -273,4 +239,48 @@ class NextPageInfo(object):
         self.uri = uri
         self.message = message
         self.systemLoginRequired = systemLoginRequired
+
+
+def password_set(context, user):
+    '''Check if a password has ever been set.
+        
+Description
+-----------
+
+We cannot check the password itself to see if the password is blank,
+because it may be hashed in all sorts of ways, so a blank password
+does no look blank. Instead this little method looks in the audit-trail
+for a profile-event that corresponds to a member setting a password.
+
+Arguments
+---------
+
+  * ``context``: The Zope context, which is used to find the data 
+    adapter.
+  * ``user``: The GroupServer CustomUser instance that is to be checked.
+
+Returns
+-------
+
+Boolean, True if a password has been set.
+
+Side Effects
+------------
+
+None.
+
+Acknowledgements
+---------------
+
+Thanks to Alice for suggesting that I trawl the audit-logs for the 
+set-password events.'''
+    q = AuditQuery(context.zsqlalchemy)
+    items = q.get_instance_user_events(user.getId(), limit=128)
+    setPasswordItems = [i for i in items 
+                        if ((i['subsystem'] == SUBSYSTEM)
+                            and (i['code'] == SET_PASSWORD))]
+    retval = bool(setPasswordItems)
+    return retval
+
+
 
