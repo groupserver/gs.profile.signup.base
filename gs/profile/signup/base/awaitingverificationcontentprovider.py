ea40eee1422cb1885e44ca9304608205bfcf527f
Michael JasonSmith <mpj17@onlinegroups.net>
2012-10-23 11:15:02 +1100

    Switching the configuration for the Awaiting Verification JavaScript Content Provider from Python to the ZCML.

--- gs/profile/signup/base/awaitingverificationcontentprovider.py
+++ gs/profile/signup/base/awaitingverificationcontentprovider.py
@@ -1,27 +1,19 @@
 # coding=utf-8
-from zope.component import createObject, provideAdapter, adapts
 from zope.pagetemplate.pagetemplatefile import PageTemplateFile
-from zope.interface import Interface, implements
-from zope.publisher.interfaces import IDefaultBrowserLayer
-from zope.contentprovider.interfaces import UpdateNotCalled, IContentProvider
-from interfaces import IGSAwaitingVerificationJavaScriptContentProvider
+from zope.contentprovider.interfaces import UpdateNotCalled
+from gs.viewlet.contentprovider import SiteContentProvider
 
 
-class GSAwaitingVerificationJavaScriptContentProvider(object):
+class GSAwaitingVerificationJavaScriptContentProvider(SiteContentProvider):
     """Content provider for the awaiting verification JavaScript."""
-    implements(IGSAwaitingVerificationJavaScriptContentProvider)
-    adapts(Interface, IDefaultBrowserLayer, Interface)
 
     def __init__(self, context, request, view):
-        self.__parent__ = self.view = view
+        s = super(GSAwaitingVerificationJavaScriptContentProvider, self)
+        s.__init__(context, request, view)
         self.__updated = False
-        self.context = context
-        self.request = request
 
     def update(self):
         self.__updated = True
-        self.siteInfo = createObject('groupserver.SiteInfo',
-          self.context)
 
     def render(self):
         if not self.__updated:
@@ -29,7 +21,3 @@ class GSAwaitingVerificationJavaScriptContentProvider(object):
 
         pageTemplate = PageTemplateFile(self.pageTemplateFileName)
         return pageTemplate(view=self)
-
-provideAdapter(GSAwaitingVerificationJavaScriptContentProvider,
-    provides=IContentProvider,
-    name="groupserver.AwaitingVerificationJavaScript")
