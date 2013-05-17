0c8d5949a4e90604ed041862fa2f74aefa98011a
Michael JasonSmith <mpj17@onlinegroups.net>
2013-05-17 11:18:44 -0700

    Upping the version number of the Set Password JS.

--- gs/profile/signup/base/browser/javascript/set_password.js
+++ gs/profile/signup/base/browser/javascript/set_password.js
@@ -1,54 +1,14 @@
 jQuery.noConflict();
 
-function GSProfilePasswordToggleVisibility (entryId, toggleId) {
-    var entry = null, 
-        toggle = null,
-        visible = false;
-
-    function set_visible () {
-        entry.attr('type', 'text');
-        visible = true;
-    }
-    
-    function set_hidden () {
-        entry.attr('type', 'password');
-        visible = false;
-    }
-
-    function get_visibility_value () {
-        return visibile;
-    }
-
-    function toggle_visibility() {
-        visible = ! visible;
-        if ( visible ) {
-            set_visible();
-        } else {
-            set_hidden();
-        }
-    }
-
-    function init () {
-        entry = jQuery(entryId);
-        toggle = jQuery(toggleId);
-        visible = Boolean(Number(toggle.val()));
-        if ( visible ) {
-            set_visible();
-        } else {
-            set_hidden();
-        }
-        toggle.change(toggle_visibility);
-        entry.focus();
-    }
-    init();
-
-    return {
-        get_visibility: function () { return get_visibility_value(); },
-    }
+function gs_profile_signup_base_password_init () {
+    var toggler = null;
+    toggler = GSProfilePasswordToggle('#form\\.password1', 
+                                      '#gs-profile-signup-base-password-toggle-widget');
 }
 
 jQuery(window).load( function () {
-    var passwordToggle = null;
-    passwordToggle = GSProfilePasswordToggleVisibility('#form\\.password1', 
-                                                       '#gs-profile-signup-base-password-toggle-widget');
+    jQuery('#form\.password1').focus();
+    gsJsLoader.with_module('/++resource++gs-profile-password-toggle-min-20130516.js', 
+                           gs_profile_signup_base_password_init);
+
 });
